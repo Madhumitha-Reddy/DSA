0ee36d93da7dd7eeef9c76ee0d118c3b32f9cc7c
Madhumitha Reddy <163890500+Madhumitha-Reddy@users.noreply.github.com>
2026-02-16 13:02:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (84.78%) - LeetSync

--- 2384-root-equals-sum-of-children/root-equals-sum-of-children.java
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.java
@@ -15,7 +15,9 @@
  */
 class Solution {
     public boolean checkTree(TreeNode root) {
-        if(root.left.val + root.right.val == root.val) return true;
+        if(root.left.val + root.right.val == root.val){
+            return true;
+        }
         return false;
     }
 }
