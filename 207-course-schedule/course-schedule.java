6b7b8553c78cd10047efc60f4f3000215ae33b8e
Madhumitha Reddy <163890500+Madhumitha-Reddy@users.noreply.github.com>
2026-04-07 10:22:01 +0530

    Time: 4 ms (92.89%) | Memory: 46.9 MB (81.76%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,42 +1,40 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> graph = new ArrayList<>();
-        for(int i=0; i<numCourses; i++){
+        for(int i=0; i < numCourses; i++){
             graph.add(new ArrayList<>());
         }
 
-        for(int[] it : prerequisites){
-            int a = it[0];
-            int b = it[1];
-
-            graph.get(b).add(a);
+        for(int[] pre : prerequisites){
+            graph.get(pre[1]).add(pre[0]);
         }
 
         int[] visited = new int[numCourses];
 
         for(int i=0; i<numCourses; i++){
-            if(!dfs(graph, visited, i)){
-                return false;
+            if(visited[i] == 0){
+                if(dfs(i, graph, visited)){
+                    return false;
+                }
             }
         }
 
         return true;
     }
 
-    boolean dfs(List<List<Integer>> graph, int[] visited, int node){
-        if(visited[node] == 1) return false;
-        if(visited[node] == 2) return true;
+    public boolean dfs(int node, List<List<Integer>> graph, int[] visited){
+        if(visited[node] == 1) return true;
+        if(visited[node] == 2) return false;
 
         visited[node] = 1;
 
-        for (int neighbor : graph.get(node)) {
-            if (!dfs(graph, visited, neighbor)) {
-                return false;
+        for(int nei : graph.get(node)){
+            if(dfs(nei, graph, visited)){
+                return true;
             }
         }
 
         visited[node] = 2;
-
-        return true;
+        return false;
     }
 }
