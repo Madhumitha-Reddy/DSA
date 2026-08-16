fe89d8a0296b25b9d8aa7770b95c1a631ad790f4
Madhumitha Reddy <163890500+Madhumitha-Reddy@users.noreply.github.com>
2026-08-16 12:44:05 +0530

    Time: 19 ms (83.45%) | Memory: 45.7 MB (77.95%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,32 +1,35 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
         for(int i=0; i<nums.length; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
             for(int j=i+1; j<nums.length; j++){
-                HashSet<Integer> set = new HashSet<>();
-                for(int k=j+1; k<nums.length; k++){
-                    long sum = (long) nums[i] + nums[j] + nums[k];
-                    long fourth = (long) target - (sum);
-                    if(fourth >= Integer.MIN_VALUE && fourth <= Integer.MAX_VALUE && set.contains((int) fourth)){
-                        List<Integer> temp = new ArrayList<>();
+                if(j != i + 1 && nums[j] == nums[j - 1]) continue;
+                int k = j + 1;
+                int l = nums.length - 1;
+                while(k < l){
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum == target){
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    temp.add(nums[l]);
 
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
-                        temp.add((int) fourth);
+                    list.add(temp);
+                    k++;
+                    l--;
+                    while(k < l && nums[k] == nums[k-1]) k++;
+                    while(k < l && nums[l] == nums[l + 1]) l--;
 
-                        Collections.sort(temp);
-
-                        if(!list.contains(temp)){
-                            list.add(temp);
-                        }
-                    }
-
-                    set.add(nums[k]);
+                }else if(sum < target)k++;
+                else l--;
                 }
+                
+
             }
         }
-
         return list;
     }
 }
