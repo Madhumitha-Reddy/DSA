76b63ffa1455afe10f6a528439330b3a161f2fbe
Madhumitha Reddy <163890500+Madhumitha-Reddy@users.noreply.github.com>
2026-09-08 16:16:59 +0530

    Time: 105 ms (46.10%) | Memory: 44.4 MB (49.66%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,14 +1,15 @@
 class Solution {
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n + 1][n + 1];
+
+        int[][] dp = new int[n + 2][n + 2];
         for(int[] num : dp){
             Arrays.fill(num, -1);
         }
-        return helper(0, n - 1, 1, 1, nums, dp);
+        return helper(0, nums.length - 1, nums, dp);
     }
 
-    int helper(int i, int j, int left, int right, int[] nums, int[][] dp){
+    int helper(int i, int j,  int[] nums, int[][] dp){
         if(i > j){
             return 0;
         }
@@ -17,12 +18,13 @@ class Solution {
             return dp[i][j];
         }
 
-        int maxi = 0;
+        int maxi = Integer.MIN_VALUE;
 
-        for(int k = i; k <= j; k++){
+        for(int k=i; k<=j; k++){
 
-
-            int coins = left * nums[k] * right + helper(i, k - 1, left, nums[k], nums, dp) + helper(k + 1, j, nums[k], right, nums, dp);
+            int left = (i == 0) ? 1 : nums[i - 1];
+            int right = (j == nums.length - 1) ? 1 : nums[j + 1];
+            int coins = left * nums[k] * right + helper(i, k - 1 , nums, dp) + helper(k + 1, j, nums, dp);
             maxi = Math.max(maxi, coins);
         }
 
