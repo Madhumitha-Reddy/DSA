684e02813c6032c3d560b8d9a213cd92daf6d0aa
Madhumitha Reddy <163890500+Madhumitha-Reddy@users.noreply.github.com>
2026-09-13 16:13:19 +0530

    Time: 2644 ms (5.01%) | Memory: 46.9 MB (22.12%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,31 +1,32 @@
 class Solution {
     public String longestPalindrome(String s) {
-
         String longest = "";
         for(int i=0; i<s.length(); i++){
-            for(int j = i + 1; j <= s.length(); j++){
-                String sub = s.substring(i, j);
+           for(int j=i; j<s.length(); j++){
+                String sub = s.substring(i, j + 1);
+                String pal = palindrome(sub, 0, sub.length()-1, "");
 
-                if(reverse(sub, 0, sub.length() - 1)){
-                    if(sub.length() > longest.length()){
-                        longest = sub;
-                    } 
+                if(!pal.equals("") && pal.length() > longest.length()){
+                    longest = pal;
                 }
-            }
+           }
         }
+
         return longest;
     }
 
-    boolean reverse(String s, int left, int right){
+    String palindrome(String s, int left, int right, String current){
 
         while(left < right){
-            if(s.charAt(left) != s.charAt(right)){
-                return false;
+
+            if(s.charAt(left) != s.charAt(right)) {
+                return "";
             }
+
             left++;
             right--;
         }
 
-        return true;
+        return s;
     }
 }
